fcd63e02a120a3ed25722b32af5ffa5fbe015080
Nicanor Orlando <nicanororlando@gmail.com>
2021-10-19 16:39:40 -0700

    Initial Commit

---
+++ Instituto Misionero/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instituto Misionero
+//
+//  Created by Nicanor Orlando on 19/10/2021.
+//  Copyright © 2021 KnorDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
